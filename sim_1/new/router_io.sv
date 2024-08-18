0278386b35cc6c3e5c98b6d1455677ea6e4e8a5a
PhuocTai03 <taidp.vn@gmail.com>
2024-08-18 20:33:36 +0700

    update interface

--- sim_1/new/router_io.sv
+++ sim_1/new/router_io.sv
@@ -1,19 +1,19 @@
-interface router_io(input bit clk);
-    logic reset_n;
-    logic [15:0] din;
-    logic [15:0] frame_n;
-    logic [15:0] valid_n;
-    logic [15:0] dout;
-    logic [15:0] frameo_n;
-    logic [15:0] valido_n;
-    clocking cb @(posedge clk);
-        output  reset_n;
-        output  din;
-        output  frame_n;
-        output  valid_n;
-        input   dout;
-        input   valido_n;
-        input   frameo_n;
-    endclocking
-    modport tb (clocking cb, output reset_n);
-endinterface: router_io
+//interface router_io_oldversion(input bit clk);
+//    logic           reset_n;
+//    logic [15:0]    din;
+//    logic [15:0]    frame_n;
+//    logic [15:0]    valid_n;
+//    logic [15:0]    dout;
+//    logic [15:0]    frameo_n;
+//    logic [15:0]    valido_n;
+//    clocking cb @(negedge clk);
+//        output  reset_n;
+//        output  din;
+//        output  frame_n;
+//        output  valid_n;
+//        input   dout;
+//        input   valido_n;
+//        input   frameo_n;
+//    endclocking
+//    modport tb (clocking cb, output reset_n);
+//endinterface: router_io_oldversion
